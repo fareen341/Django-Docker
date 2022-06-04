1090977c5579148f41a87af0a8a6a12eabb1fd87
fareen341 <59610617+fareen341@users.noreply.github.com>
2022-06-04 14:26:37 +0530

    Update views.py

--- HelloWorld/HelloWorldApp/views.py
+++ HelloWorld/HelloWorldApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse("Hello world from django v3 ... from ansible script using CI/CD pipeline ...v2.0!!")
+    return HttpResponse("Hello world from django v3 !!")
