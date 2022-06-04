34a96bfccaa62ca07a84e215be32712bf5a31be1
fareen341 <59610617+fareen341@users.noreply.github.com>
2022-06-04 10:16:44 +0530

    Update views.py

--- HelloWorld/HelloWorldApp/views.py
+++ HelloWorld/HelloWorldApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse("Hello world from django!!")
+    return HttpResponse("Hello world from django v2!!")
