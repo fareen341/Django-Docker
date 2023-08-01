de9f472e0b79b608153e4a8294ee98101c063ff0
fareen341 <59610617+fareen341@users.noreply.github.com>
2023-08-01 22:06:52 +0530

    Update views.py

--- HelloWorld/HelloWorldApp/views.py
+++ HelloWorld/HelloWorldApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse("Hello world from django with CI/CD pipeline v1.1!!")
+    return HttpResponse("Hello world from django with CI/CD pipeline v1.2!!")
